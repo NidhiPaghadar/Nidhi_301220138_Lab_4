11951f72069ee7c91c90f99d46c01f1bbe8a15b8
Nidhi Paghadar <106948288+NidhiPaghadar@users.noreply.github.com>
2023-04-13 22:09:19 -0400

    Add project files.

---
+++ Nidhi_301220138_Lab_4/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Nidhi_301220138_Lab_4
+{
+    public static class StringLibrary
+    {
+        public static bool StartsWithUpper(this string? str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
